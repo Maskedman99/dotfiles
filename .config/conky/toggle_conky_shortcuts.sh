bc0bd5dff60a7a6a8cadedfc0b466ddec01805e2
maskedman <rohitmpaul@gmail.com>
2020-12-20 12:58:36 +0530

    rename files

--- .config/conky/toggle_conky_shortcuts.sh
+++ .config/conky/toggle_conky_shortcuts.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-runningConkys=$(pgrep -a conky | awk '/shortcuts/{print $1}')
+runningConkys=$(pgrep -a conky | awk '/_conky_shortcuts/{print $1}')
 
 # if runningConkys is empty
 if [[ -z "$runningConkys" ]]; then
-    conky -q -c $HOME/.config/conky/conky_i3_shortcuts.lua &
-    conky -q -c $HOME/.config/conky/conky_nvim_shortcuts.lua &
+    conky -q -c $HOME/.config/conky/i3_conky_shortcuts.lua &
+    conky -q -c $HOME/.config/conky/nvim_conky_shortcuts.lua &
 else
     echo "$runningConkys" | xargs -n 1 kill -15
 fi
