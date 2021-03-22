ee7b70cb6f99afac7ec1e5d33c512a5b5720ea0f
maskedman <rohitmpaul@gmail.com>
2021-03-22 19:02:09 +0530

    open external programs quietly in background

--- .config/newsboat/scripts/urlchooser.sh
+++ .config/newsboat/scripts/urlchooser.sh
@@ -3,8 +3,8 @@
 EXT=$(echo $1 | awk -F'/' '{print $NF}' | awk -F. '{print $NF}')
 
 case $EXT in
-	"jpg"* | "png"* | "jpeg"* | "JPG"* | "svg") magick display -monitor $1;;
-	"gif") mpv -loop $1;;
-	"mp4") mpv $1;;
-	*) firefox $1;;
+	"jpg"* | "png"* | "jpeg"* | "JPG"* | "svg") feh -q $1 >/dev/null 2>&1 &;;
+	"gif") mpv -loop $1 >/dev/null 2>&1 &;;
+	"mp4") mpv -quiet $1 >/dev/null 2>&1 &;;
+	*) firefox $1 >/dev/null 2>&1 &;;
 esac
