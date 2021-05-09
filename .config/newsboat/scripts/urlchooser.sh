120c2349260d74b9aa992ea3611945868d22c8a3
maskedman <rohitmpaul@gmail.com>
2021-05-09 14:02:21 +0530

    mpv: play gif and webm in loop, pause jpeg and png

--- .config/newsboat/scripts/urlchooser.sh
+++ .config/newsboat/scripts/urlchooser.sh
@@ -4,7 +4,6 @@ EXT=$(echo $1 | awk -F'/' '{print $NF}' | awk -F. '{print $NF}')
 
 case $EXT in
 	"jpg"* | "png"* | "jpeg"* | "JPG"* | "svg") feh -q $1 >/dev/null 2>&1 &;;
-	"gif") mpv -loop $1 >/dev/null 2>&1 &;;
-	"mp4") mpv -quiet $1 >/dev/null 2>&1 &;;
+	"gif" | "mp4") mpv -quiet $1 >/dev/null 2>&1 &;;
 	*) firefox $1 >/dev/null 2>&1 &;;
 esac
