7d7692e4894a3e13b1512e51a35050caed65f7dc
Maskedman99 <rohitmpaul@gmail.com>
2021-06-01 13:35:01 +0530

    move swaymatrixlock.sh to scripts directory

--- .config/scripts/swaymatrixlock.sh
+++ .config/scripts/swaymatrixlock.sh
@@ -1,8 +1,10 @@
+#!/bin/dash
+
 CURRENTWORKSPACE=$(swaymsg -t get_tree | grep current_workspace | cut -d'"' -f 4)
 
 swaymsg workspace 0
 swaymsg bar mode invisible
-swaymsg "exec alacritty -e zsh -c 'sleep 0.1 && cmatrix -b'" && swaylock && killall cmatrix
+swaymsg "exec alacritty -e dash -c 'sleep 0.1 && cmatrix -b'" && swaylock && killall cmatrix
 swaymsg bar mode toggle
 swaymsg "workspace "$CURRENTWORKSPACE""
 
